984f1eb47ef14b1b3aeead6694628eee063a669d
ARXIMED1 <114014318+ARXIMED1@users.noreply.github.com>
2023-01-14 15:25:56 +0300

    nothing

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    [SerializeField] private Health playerHealth;
+    [SerializeField] private HealthPirate playerHealth;
     [SerializeField] private Image totalhealthBar;
     [SerializeField] private Image currenthealthBar;
     private void Start()
